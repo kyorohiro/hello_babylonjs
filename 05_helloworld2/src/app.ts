e3fd9b1c798ae3353935875fdb393635f7de6244
kyorohiro <kyorohiro@gmail.com>
2023-04-10 03:28:43 +0900

    savev temporary

--- 05_helloworld2/src/app.ts
+++ 05_helloworld2/src/app.ts
@@ -3,39 +3,52 @@ import "@babylonjs/inspector";
 import "@babylonjs/loaders/glTF";
 import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder } from "@babylonjs/core";
 
+enum State { START = 0, GAME = 1, LOSE = 2, CUTSCENE = 3 }
+
 class App {
+    private _canvas: HTMLCanvasElement;
+    private _scene: Scene;
+    private _engine: Engine;
+    private _state: State = State.START;
+
     constructor() {
         // create the canvas html element and attach it to the webpage
-        var canvas = document.createElement("canvas");
-        canvas.style.width = "100%";
-        canvas.style.height = "100%";
-        canvas.id = "gameCanvas";
-        document.body.appendChild(canvas);
+        this._canvas = document.createElement("canvas");
+        this._canvas.style.width = "100%";
+        this._canvas.style.height = "100%";
+        this._canvas.id = "gameCanvas";
+        document.body.appendChild(this._canvas);
 
         // initialize babylon scene and engine
-        var engine = new Engine(canvas, true);
-        var scene = new Scene(engine);
+        this._engine = new Engine(this._canvas, true);
+        this._scene = new Scene(this._engine);
 
-        var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
-        camera.attachControl(canvas, true);
-        var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
-        var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
+        var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), this._scene);
+        camera.attachControl(this._canvas, true);
+        var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), this._scene);
+        var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, this._scene);
 
         // hide/show the Inspector
         window.addEventListener("keydown", (ev) => {
             // Shift+Ctrl+Alt+I
-            if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.key === 'i') {
-                if (scene.debugLayer.isVisible()) {
-                    scene.debugLayer.hide();
+            console.log('--keydown')
+            console.log(ev.shiftKey)
+            console.log(ev.ctrlKey)
+            console.log(ev.key )
+            console.log(ev.code )
+            console.log(ev.altKey  )
+            if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.code === 'KeyI') {
+                if (this._scene.debugLayer.isVisible()) {
+                    this._scene.debugLayer.hide();
                 } else {
-                    scene.debugLayer.show();
+                    this._scene.debugLayer.show();
                 }
             }
         });
 
         // run the main render loop
-        engine.runRenderLoop(() => {
-            scene.render();
+        this._engine.runRenderLoop(() => {
+            this._scene.render();
         });
     }
 }
