cd72ce4c707b26827133f7fb64e9f2da1a845cde
kyorohiro <kyorohiro@gmail.com>
2023-04-17 03:27:17 +0900

    save

---
+++ 05_helloworld2/src/environment.ts
@@ -0,0 +1,18 @@
+import { Scene, Mesh, Vector3 } from "@babylonjs/core";
+
+class Environment {
+    private _scene: Scene;
+
+    constructor(scene: Scene) {
+        this._scene = scene;
+    }
+
+    public async load() {
+        var ground = Mesh.CreateBox("ground", 24, this._scene);
+        ground.scaling = new Vector3(1,.02,1);
+    }
+}
+
+export {
+    Environment
+}
